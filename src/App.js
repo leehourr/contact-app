b03e4afe0c01b58ded55a6d9ee76fe8065c0db0a
Lee Hour <lhour999@gmail.com>
2022-10-25 01:40:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import AllContact from "./pages/AllContacts";
+// import AddContact from "./pages/AddContact";
+// import ContactDetails from "./pages/ContactDetails";
+// import EditContact from "./pages/EditContact";
+import { MainNav } from "./components/Ui/MainNav";
+//import { Contact } from "./components/Contact/Contact";
+import { ContactList } from "./components/Contact/ContactList";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <RootLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/contact",
+//         element: <AllContact />,
+//         children: [
+//           {
+//             path: ":id",
+//             element: <ContactDetai />,
+//             loader: blogPostLoader,
+//           },
+//         ],
+//       },
+//       {
+//         path: "/contact/new",
+//         element: <AddContact />,
+//         action: newPostAction,
+//       },
+//       {
+//         path: "/contact/edit",
+//         element: <EditContact />,
+//         action: newPostAction,
+//       },
+//     ],
+//   },
+// ]);
 
 function App() {
+  //return <RouterProvider router={router} />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center align-text-top flex-col bg-gray-800 py-12 px-4 sm:px-6 lg:px-8 ">
+      <div className="max-w-fit space-y-8 p-0" >
+        <h2 className="mt-6 mb-10 text-center text-3xl font-extrabold text-cyan-400 cursor-pointer">
+          CONTACT
+        </h2>
+      </div>
+      {/* <MainNav /> */}
+      <ContactList  />
     </div>
   );
 }
